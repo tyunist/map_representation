f291e85e36e5691a2750511950654a9c292f7dbc
tyunist <tynguyen@unist.ac.kr>
2017-10-30 23:26:06 -0400

    Complete simple_grid

--- code/demo.py
+++ code/demo.py
@@ -10,8 +10,29 @@ import hilbert_map as hm
 import util 
 
 # online drawing 
-plt.ion()
+#plt.ion()
+def simple_grid(data, fname, resolution=0.1):
+  poses = data['poses']
+  scans = data['scans']
+  xlim, ylim = util.bounding_box(poses, 40.0)
+  x_count = int(math.ceil((xlim[1] - xlim[0]) / resolution))
+  y_count = int(math.ceil((ylim[1] - ylim[0]) / resolution))
+  map = np.zeros([y_count, x_count]) 
+  plt.figure()
+  for data, label in util.data_generator(poses, scans):
+    #pdb.set_trace()  
+    for i in range(len(label)):
+      x = int((data[i][0] - xlim[0])/(xlim[1] - xlim[0])*(x_count-1))
+      y = int((data[i][1] - ylim[0])/(ylim[1] - ylim[0])*(y_count-1))
+      occupied = label[i] 
+      map[y, x] = occupied
+    print '<x, y>:', x, y, 'limit:', x_count, y_count 
 
+  plt.imshow(map, cmap='gray')
+  plt.title('Grid Occupancy Map,' + fname.split('.')[0]) 
+  plt.savefig(fname)
+  plt.show()
+ 
 def generate_hilbert_map(model, resolution, limits, fname, verbose=True, percent_map=100):
     """Generates a grid map by querying the model at cell locations.
 
@@ -62,7 +83,7 @@ def generate_hilbert_map(model, resolution, limits, fname, verbose=True, percent
     plt.pause(0.05)
 
 
-def train_sparse_hm(data, components, gamma, distance_cutoff, map_resolution=None):
+def train_sparse_hm(data, components, gamma, distance_cutoff, map_resolution=None, online_map=False):
 	"""Trains a hilbert map model using the sparse feature.
 
 	:param data the dataset to train on
@@ -91,69 +112,73 @@ def train_sparse_hm(data, components, gamma, distance_cutoff, map_resolution=Non
 		# print 'data', data, 'label', label 
 		percent_map = count/float(len(poses)) * 100
 		model.add(data, label)
-		# sys.stdout.write("\rTraining model:{: 6.2f}%".format(percent_map))
-		# sys.stdout.flush()
+		sys.stdout.write("\rTraining model:{: 6.2f}%".format(percent_map))
+		sys.stdout.flush()
 		sys.stdout.write("\rold_percent_map:{: 6.2f}%".format(old_percent_map))
 		sys.stdout.flush()
 		print('\n...current %%: ', percent_map)
-		if percent_map > old_percent_map + 1:
+		if (percent_map > old_percent_map + 30 and online_map) or percent_map == 100:
 			print('...generating map')
 			generate_hilbert_map(
 			  model,
 			  map_resolution,
 			  [xlim[0], xlim[1], ylim[0], ylim[1]],
-			  "hilbert_map.png"
+			  "hilbert_map" + str(percent_map) + " .png", 
+        percent_map 
 			)
 			old_percent_map = percent_map
 		count += 1 
 	 
 
 def main():
-	# Set command line argument parsing 
-	parser = argparse.ArgumentParser(description='Ty Demo Hilbert Map')
-	parser.add_argument("--log_dir", help="Logfile in CARMEN format to process",\
-		default="/media/sf_cogntivive_school/hilbert_map/datasets/intel.gfs.log")
+  # Set command line argument parsing 
+  parser = argparse.ArgumentParser(description='Ty Demo Hilbert Map')
+  parser.add_argument("--log_dir", help="Logfile in CARMEN format to process",\
+    #default="../datasets/intel.gfs.log")
+    default="map_representation/datasets/fr-campus-20040714.carmen.gfs.log")
 
-	parser.add_argument("--feature", choices=["sparse", "fourier", "nystroem"],\
-		help="The feature to use", \
-		default="sparse")
-	parser.add_argument("--components", \
-		default=1000,\
-		type=int,\
-		help="Number of components used with the feature")
-	parser.add_argument("--gamma", \
-		default=1,\
-		type=float,\
-		help="Gamma value used in the RBF kernel")
-	parser.add_argument("--distance_cutoff",\
-		default=0.001,\
-		type=float,\
-		help="Value below which a kernel value will be set to 0")
-	parser.add_argument("--resolution",\
-		default=0.1,\
-		type=float,\
-		help="Grid cell resolution of the map")
+  parser.add_argument("--feature", choices=["sparse", "fourier", "nystroem"],\
+    help="The feature to use", \
+    default="sparse")
+  parser.add_argument("--components", \
+    default=1000,\
+    type=int,\
+    help="Number of components used with the feature")
+  parser.add_argument("--gamma", \
+    default=1,\
+    type=float,\
+    help="Gamma value used in the RBF kernel")
+  parser.add_argument("--distance_cutoff",\
+    default=0.001,\
+    type=float,\
+    help="Value below which a kernel value will be set to 0")
+  parser.add_argument("--resolution",\
+    default=0.1,\
+    type=float,\
+    help="Grid cell resolution of the map")
 
-	args = parser.parse_args()
+  args = parser.parse_args()
 
-	# Load the data and split it into training and testing data 
+  # Load the data and split it into training and testing data 
 
-	train_data, test_data = util.create_test_train_split(args.log_dir, 0.1)	
+  train_data, test_data = util.create_test_train_split(args.log_dir, 0.1)	
+  # Create a simple grid map 
+  #simple_grid(train_data, 'fr-campus_simple_grid.png')
+  
+  # Train the desired model on the data 
+  if args.feature == 'sparse':
+    model = train_sparse_hm(train_data, args.components, args.gamma, args.distance_cutoff, args.resolution)
+  # else:
+  # 	model = train_incremental_hm(train_data, args.components, args.gamma, args.feature, visual_map=False)
 
-	# Train the desired model on the data 
-	if args.feature == 'sparse':
-		model = train_sparse_hm(train_data, args.components, args.gamma, args.distance_cutoff, args.resolution)
-	# else:
-	# 	model = train_incremental_hm(train_data, args.components, args.gamma, args.feature, visual_map=False)
-
-	# Produce grid map based on the trained model
-	xlim, ylim = util.bounding_box(train_data["poses"], 10.0)
-	generate_hilbert_map(
-	        model,
-	        args.resolution,
-	        [xlim[0], xlim[1], ylim[0], ylim[1]],
-	        "hilbert_map.png"
-	)
+  # Produce grid map based on the trained model
+  xlim, ylim = util.bounding_box(train_data["poses"], 10.0)
+  generate_hilbert_map(
+          model,
+          args.resolution,
+          [xlim[0], xlim[1], ylim[0], ylim[1]],
+          "hilbert_map.png"
+  )
 
 if __name__=="__main__":
 	main()
